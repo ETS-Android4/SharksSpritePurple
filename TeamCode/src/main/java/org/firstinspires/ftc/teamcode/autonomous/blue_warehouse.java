200ed34e1851b6ae7a9e91102e351e0c9cd5289f
JonathanLi13090 <65463483+JonathanLi13090@users.noreply.github.com>
2022-03-11 21:37:13 -0800

    blue_warehouse init

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/blue_warehouse.java
@@ -0,0 +1,100 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import android.os.Build;
+import androidx.annotation.RequiresApi;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import java.util.*;
+import java.util.function.BinaryOperator;
+
+import org.firstinspires.ftc.teamcode.RobotMain358;
+
+@Autonomous
+public class blue_warehouse extends RobotMain358 {
+
+    private boolean done = false;
+    int FINAL_POSITION;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void runOpMode() throws InterruptedException {
+
+        CHASSIS_INITIALIZE();
+
+        while (!opModeIsActive()){
+            List<Integer> detected = new ArrayList<>();
+            for (int i = 0; i < 20; i++) {
+                detected.add(DETECT_POSITION_BLUE());
+                sleep(10);
+            }
+
+            FINAL_POSITION = detected.stream().
+                    reduce(BinaryOperator.maxBy((o1,o2) -> Collections.frequency(detected, o1) -
+                            Collections.frequency(detected, o2))).orElse(3);
+            detected.clear();
+
+            telemetry.addData("Position Detected", FINAL_POSITION);
+            telemetry.update();
+
+            FINAL_POSITION = DETECT_POSITION_BLUE();
+        }
+
+        waitForStart();
+        while (opModeIsActive() && !done) {
+
+            forward(36, 0.5);
+
+            slideAuto();
+
+            blackBox.setPosition(0.086); sleep(1300);
+            blackBox.setPosition(0.439); sleep(500);
+            blackBox.setPosition(0.086); sleep(1300);
+            blackBox.setPosition(0.439); sleep(500);
+
+            forward(-30, 0.5);
+            turn(-90, 0.5);
+            strafe(10, 0.5);
+            
+
+            FINAL_POSITION = 0;
+            slideAuto();
+
+            strafe(40, 0.5);
+            strafe(5, 0.3);
+            forward(13, 0.5);
+
+            done = true;
+        }
+    }
+
+    public void slideAuto(){
+        /**
+         * LEVEL 1 = 600 - 50
+         * LEVEL 2 = 1200 - 50
+         * MAX / LEVEL 3 = 1800 - 50
+         * */
+
+        telemetry.addData("slide", "yay!");
+        telemetry.update();
+
+        // set target position based on sensed position
+        if (FINAL_POSITION == 0){
+            slideMotor.setTargetPosition(50);
+        }else if (FINAL_POSITION == 1) {
+            strafeRightAuto(10, 0.3, 9);
+            slideMotor.setTargetPosition(550);
+        } else if (FINAL_POSITION == 2) {
+            strafeRightAuto(10, 0.3, 8);
+            slideMotor.setTargetPosition(1150);
+        } else if (FINAL_POSITION == 3) {
+            strafeRightAuto(10, 0.3, 7);
+            slideMotor.setTargetPosition(1750);
+        }
+
+        // set power and mode
+        slideMotor.setPower(1);
+        slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        while (slideMotor.isBusy()){}
+
+    }
+}
